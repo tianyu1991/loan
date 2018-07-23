a55a0bdd032b657701dbd2d943711b61f1bde598
tianyu liu <hulan1991@gmail.com>
2018-07-22 22:32:04 -0400

    Update log.r

--- log.r
+++ log.r
@@ -1,28 +1,32 @@
+data<-read.csv("LoanStats3d.csv")
+
 grep("total_il_high_credit_limit", colnames(data))
+
 ##only individual
 data2<-subset(data,application_type=="Individual"& grade!="")
-data2<-subset(data2,select=c(loan_amnt,int_rate,grade,dti,open_acc,inq_last_6mths,acc_open_past_24mths))
+data2<-subset(data2,select=c(loan_amnt,int_rate,grade,dti,open_acc,inq_last_6mths,acc_open_past_24mths,emp_length,annual_inc,revol_bal))
 
+##clean data
 int<-as.character(data2$int_rate)
 int2<-gsub("%", "", int)
 data2$int_rate<-as.numeric(int2)
 
 apply(data2,function(x) sum(is.na(x)))
 
+##linear regression
 b<-lm(grade2~loan_amnt+int_rate+inq_last_6mths+acc_open_past_24mths+annual_inc,data4)
 summary(b)
-##choose varaible loan_amnt, int_rate,acc_open_past_24mths
+##choose loan_amnt, int_rate,acc_open_past_24mths
 
+##divided into train/test set
 n=nrow(data2)
-t1=sample(1:n,size=(0.9*n),replace=FALSE,prob=NULL)
-train=data2[t1,]
-test=data2[-t1,]
+t=sample(1:n,size=(0.9*n),replace=FALSE,prob=NULL)
+train=data2[t,]
+test=data2[-t,]
 
-grade2<-as.numeric(data2$grade)
-
+##logistic model
 library(nnet)
 b<-multinom(grade~loan_amnt+int_rate+acc_open_past_24mths,data=train)
-summary(b)
 
 fitted.results <- predict(b,newdata=test,type='probs')
 
@@ -42,10 +46,6 @@ for(i in 1:nrow(test)){
 }
 
  fitted.results2<-as.data.frame(fitted.results)
-t_result<-cbind(test,fitted.results2$p_grade)
-
-      
-       fitted.results2<-as.data.frame(fitted.results)
 t_result<-cbind(test,fitted.results2$p_grade)
 t_result$grade2<-as.numeric(t_result$grade)-1
 t_result$re<-ifelse(t_result$grade2==t_result[,8],0,1)
